a8c356a6bf3210d4538781c2617022b7a5c74605
Jason Gross <jgross@mit.edu>
2024-06-29 16:59:44 -0700

    WIP

--- eBPF/debug_xdp.py
+++ eBPF/debug_xdp.py
@@ -9,27 +9,8 @@ BPF_HASH(blocked_ips, __u32, __u32, 1); // Simplified to just store a single IP
 
 int xdp_prog(struct xdp_md *ctx)
 {
-    void *data_end = (void *)(long)ctx->data_end;
-    void *data = (void *)(long)ctx->data;
-    struct ethhdr *eth = data;
 
-    if ((void *)eth + sizeof(*eth) > data_end)
-        return XDP_PASS;
-
-    if (eth->h_proto != __constant_htons(ETH_P_IP))
-        return XDP_PASS;
-
-    struct iphdr *ip = (void *)eth + sizeof(*eth);
-    if ((void *)ip + sizeof(*ip) > data_end)
-        return XDP_PASS;
-
-    __u32 src_ip = ip->saddr;
-    __u32 *blocked = bpf_map_lookup_elem(&blocked_ips, &src_ip);
-
-    if (blocked)
-        return XDP_DROP;
-
-    return XDP_PASS;
+    return XDP_DROP;
 }
 
 char __license[] __attribute__((section("license"), used)) = "GPL";
