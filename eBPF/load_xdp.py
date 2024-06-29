24eb022b14b02ef6a1fea8f1d31b2234dbd246f8
Jason Gross <jgross@mit.edu>
2024-06-29 16:19:16 -0700

    inline

--- eBPF/load_xdp.py
+++ eBPF/load_xdp.py
@@ -3,7 +3,36 @@ from bcc import BPF
 import pyroute2
 
 # Define the BPF program
-bpf = BPF(src_file="xdp_prog.c", debug=4)
+bpf = BPF(text=r"""#include <linux/bpf.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+
+BPF_HASH(blocked_ips, __u32, __u32, 1); // Simplified to just store a single IP
+
+int xdp_prog(struct xdp_md *ctx)
+{
+    void *data_end = (void *)(long)ctx->data_end;
+    void *data = (void *)(long)ctx->data;
+    struct ethhdr *eth = data;
+
+    if ((void *)eth + sizeof(*eth) > data_end)
+        return XDP_PASS;
+
+    if (eth->h_proto != __constant_htons(ETH_P_IP))
+        return XDP_PASS;
+
+    struct iphdr *ip = (void *)eth + sizeof(*eth);
+    if ((void *)ip + sizeof(*ip) > data_end)
+        return XDP_PASS;
+
+    __u32 src_ip = ip->saddr;
+    __u32 *blocked = bpf_map_lookup_elem(&blocked_ips, &src_ip);
+
+    if (blocked)
+        return XDP_DROP;
+
+    return XDP_PASS;
+}""", debug=4)
 
 # Attach XDP program to the network interface
 device = "eth0"  # Change this to your network interface
